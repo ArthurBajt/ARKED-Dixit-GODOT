94633b87a966a9945d2fd3292b0f9a33eb32ba3b
Luxem <luxem192@gmail.com>
2021-04-11 16:04:04 +0200

    Update de network
    Dîtes moi si vous rencontrez des bugs

--- Scripts/Network.gd
+++ Scripts/Network.gd
@@ -70,16 +70,10 @@ remote func _lobby_declareUtilisateur(idUtilisateur: int):
 	""" Quand un utilisateur se déclare,
 	le serveur signal a tt les utilisateur déjà présents
 	qu'un nv Utilisateur s'est connecté."""
-	for usId in utilisateurs:
-		if usId != 1:
-			rpc_id(usId, "_lobby_ajouteUtilisateur", idUtilisateur,dataStruct.duplicate())
-			rpc_id(idUtilisateur, "_lobby_ajouteUtilisateur", usId, utilisateurs[usId])
-		else:
-			_lobby_ajouteUtilisateur(idUtilisateur, dataStruct.duplicate())
-			rpc_id(idUtilisateur, "_lobby_ajouteUtilisateur", 1, utilisateurs[1])
+	rpc("_lobby_ajouteUtilisateur", idUtilisateur, dataStruct.duplicate())
+	rpc("_lobby_ajouteUtilisateur", 1, utilisateurs[1])
 
-
-remote func _lobby_ajouteUtilisateur(idUtilisateur: int, curentData: Dictionary = {}):
+remotesync func _lobby_ajouteUtilisateur(idUtilisateur: int, curentData: Dictionary = {}):
 	""" Le serveur a declarer l'arrivee d'un nv Utilisateur
 	ou
 	Nous somme un client arrivant sur le serveur
@@ -96,23 +90,17 @@ remote func _lobby_ajouteUtilisateur(idUtilisateur: int, curentData: Dictionary 
 func lobby_setStatu(statu: bool):
 	""" Permet a un client de changer son statu, si il est pret ou nn."""
 	self.data.estPret = statu
-	
-	if id != 1:
-		rpc_id(1, "_lobby_declareStatu", id, statu)
-	else:
-		_lobby_declareStatu(id, statu)
+	rpc("_lobby_declareStatu", id, statu)
 
 
-remote func _lobby_declareStatu(idUtilisateur: int, statu: bool):
+
+remotesync func _lobby_declareStatu(idUtilisateur: int, statu: bool):
 	""" Permet au serveur de mettre a jour le statu d'un utilisateur pour tt les autres."""
-	for usId in utilisateurs:
-		if usId != 1:
-			rpc_id(usId, "_lobby_appliquerStatu", idUtilisateur, statu)
-		else:
-			_lobby_appliquerStatu(idUtilisateur, statu)
+	rpc("_lobby_appliquerStatu", idUtilisateur, statu)
 
 
-remote func _lobby_appliquerStatu(idUtilisateur: int, statu: bool):
+
+remotesync func _lobby_appliquerStatu(idUtilisateur: int, statu: bool):
 	""" change le statu d'un joueur"""
 	if (not "estPret" in utilisateurs[idUtilisateur]) or (utilisateurs[idUtilisateur].estPret != statu):
 		emit_signal("nvStatuUtilisateur", idUtilisateur, statu)
@@ -124,15 +112,11 @@ remote func _lobby_appliquerStatu(idUtilisateur: int, statu: bool):
 
 func lobby_lancerPartie():
 	""" Permet a l'hote de la partie de démarer le jeu pour tt les utilisateurs"""
-	if id == 1:
-		if _peutLancerPartie():
-			_lobby_lancePartie()
-			for usId in utilisateurs:
-				if usId != 1:
-					rpc_id(usId, "_lobby_lancePartie")
+	if id == 1 and _peutLancerPartie():
+		rpc("_lobby_lancePartie")
+#####ICI LORIS C'EST PEUT ETRE LA QUE CA VA BUGGER######
 
-
-remote func _lobby_lancePartie():
+remotesync func _lobby_lancePartie():
 	""" Signal a tt les utilisateurs du lobby que la partie commence."""
 	emit_signal("partieLancee")
 
@@ -157,19 +141,13 @@ signal JoueursDansPartie
 
 func partie_setChargee():
 	"""Un est appelée quand un joueur a charger la scenen de dela partie."""
-	if id != 1:
-		rpc_id(1, "_partie_declareChargee", id)
-	else:
-		_partie_declareChargee(1)
+	rpc("_partie_declareChargee", id)
 
 
-remote func _partie_declareChargee(idJoeuur: int):
+remotesync func _partie_declareChargee(idJoeuur: int):
 	""" """
-	for usId in utilisateurs:
-		if usId != 1:
-			rpc_id(usId, "_partie_appliqueChargee", idJoeuur)
-		else:
-			_partie_appliqueChargee(idJoeuur)
+	rpc("_partie_appliqueChargee", idJoeuur)
+
 
 
 remote func _partie_appliqueChargee(idJoueur: int):
@@ -195,14 +173,11 @@ func _sontJoueursDansPartie()->bool:
 signal joueurApiocherCarte(id, carte)
 
 func joueurPioche(idJoueur: int, carte: String):
-	for usId in utilisateurs:
-		if usId != 1:
-			rpc_id(usId, "_joueurPiocheCarte", idJoueur, carte)
-		else:
-			_joueurPiocheCarte(idJoueur, carte)
+	rpc("_joueurPiocheCarte", idJoueur, carte)
 
 
-remote func _joueurPiocheCarte(idJoueur: int, carte: String):
+
+remotesync func _joueurPiocheCarte(idJoueur: int, carte: String):
 	if idJoueur == id:
 		self.data.main = self.data.main + [carte]
 	utilisateurs[idJoueur].main = utilisateurs[idJoueur].main + [carte]
@@ -214,21 +189,14 @@ remote func _joueurPiocheCarte(idJoueur: int, carte: String):
 signal JoueurPoseCarte(idJoueur, nomCarte)
 
 func posercarte(idJoueur: int, carte: String):
-	if id != 1:
-		rpc_id(1, "declarePoseCarte", idJoueur, carte)
-	else:
-		declarePoseCarte(idJoueur, carte)
+	rpc("declarePoseCarte", idJoueur, carte)
 
 
-remote func declarePoseCarte(idJoueur: int, carte: String):
-	for usId in utilisateurs:
-		if usId != 1:
-			rpc_id(usId, "appliquePoseCarte", idJoueur, carte)
-		else:
-			appliquePoseCarte(idJoueur, carte)
+remotesync func declarePoseCarte(idJoueur: int, carte: String):
+	rpc("appliquePoseCarte", idJoueur, carte)
 
 
-remote func appliquePoseCarte(idJoueur: int, carte: String):
+remotesync func appliquePoseCarte(idJoueur: int, carte: String):
 	if idJoueur == self.id:
 		self.data.cartesPlateau[idJoueur] = carte
 		self.data.main.erase(carte)
