ae22ea5d230a062f65661765e3a868d8527272da
Léo Laffont <endolodreams057@gmail.com>
2021-04-23 19:12:35 +0200

    sdc

--- Scripts/Globals.gd
+++ Scripts/Globals.gd
@@ -7,10 +7,10 @@ enum EtatJoueur {
 				SELECTION_CARTE_THEME, 		# Conteur			0
 				CHOIX_THEME,				# Conteur			1
 				ATTENTE_CHOIX_THEME,		# Joueur			2
-				ATTENTE_SELECTIONS,			# Conteur & Joueur	3
-				SELECTION_CARTE,			# Joueur			4
-				ATTENTE_VOTES,				# Conteur & Joueur	5
-				VOTE,						# Joueur			6
+				SELECTION_CARTE,			# Joueur			3
+				ATTENTE_SELECTIONS,			# Conteur & Joueur	4
+				VOTE,						# Joueur			5
+				ATTENTE_VOTES,				# Conteur & Joueur	6
 				VOIR_RESULTAT,				# Conteur & Joueur	7
 				ATTENTE_PROCHAINE_MANCHE	# Conteur & Joueur	8
 				}
