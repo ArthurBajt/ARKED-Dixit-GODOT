d97ec22290095c6a5a511d74d5537c223b359907
Arthur <arthur.bajt@gmail.com>
2021-05-19 10:40:48 +0200

    fix retour de ligne

--- Scenes/MenuPrincipal/PanelJouer.gd
+++ Scenes/MenuPrincipal/PanelJouer.gd
@@ -93,19 +93,22 @@ func _on_ButtonRejoindre_pressed():
 
 
 func _on_EditPseudo_text_changed():
-	self.editPseudo.text = self.editPseudo.text.replace("\n", "")
+	if "\n" in self.editPseudo.text:
+		self.editPseudo.text = self.editPseudo.text.replace("\n", "")
 	self.verifPseudo()
 	self.verifIp()
 
 
 func _on_EditIp_text_changed():
-	self.editIp.text = self.editIp.text.replace("\n", "")
+	if "\n" in self.editIp.text:
+		self.editIp.text = self.editIp.text.replace("\n", "")
 	self.verifPseudo()
 	self.verifIp()
 
 
 func _on_IpHost_text_changed():
-	self.IpHost.text = self.IpHost.text.replace("\n", "")
+	if "\n" in self.IpHost.text:
+		self.IpHost.text = self.IpHost.text.replace("\n", "")
 
 
 func _on_ButtonHost_pressed():
@@ -116,6 +119,8 @@ func _on_ButtonHost_pressed():
 
 func _on_ButtonFermer_pressed():
 	self.visible = false
+
+
 
 
 
